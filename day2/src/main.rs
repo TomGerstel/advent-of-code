020310d729f41e896ad640d36ad286b880a7e13e
SirBlueRabbit <68923537+SirBlueRabbit@users.noreply.github.com>
2023-12-05 11:01:57 +0100

    day 2 part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -2,16 +2,16 @@ use std::{fs, str};
 
 struct CubeGame {
     id: u32,
-    cubes: Vec<[usize; 3]>,
+    cubes: Vec<[u32; 3]>,
 }
 
 fn main() {
     let input = fs::read("./input.txt").unwrap();
     let result1 = part1(&input);
-    //let result2 = part2(&input);
+    let result2 = part2(&input);
 
     println!("The result of part 1 is: {result1}");
-    //println!("The result of part 2 is: {result2}");
+    println!("The result of part 2 is: {result2}");
 }
 
 fn part1(input: &[u8]) -> u32 {
@@ -22,6 +22,16 @@ fn part1(input: &[u8]) -> u32 {
         .map(CubeGame::parse)
         .filter(|cubegame| cubegame.is_possible([12, 13, 14]))
         .map(|cubegame| cubegame.id)
+        .sum()
+}
+
+fn part2(input: &[u8]) -> u32 {
+    let chars = str::from_utf8(input).unwrap();
+
+    chars
+        .split('\n')
+        .map(CubeGame::parse)
+        .map(|cubegame| cubegame.power())
         .sum()
 }
 
@@ -41,7 +51,7 @@ impl CubeGame {
                 let mut colours = [0, 0, 0];
                 hand.split(", ").for_each(|colour_amount| {
                     let (amount, colour) = colour_amount.split_once(' ').unwrap();
-                    let amount = amount.parse::<usize>().unwrap();
+                    let amount = amount.parse::<u32>().unwrap();
                     let colour_id = match colour {
                         "red" => 0,
                         "green" => 1,
@@ -57,10 +67,21 @@ impl CubeGame {
         CubeGame { id, cubes }
     }
 
-    fn is_possible(&self, colours: [usize; 3]) -> bool {
+    fn is_possible(&self, colours: [u32; 3]) -> bool {
         self.cubes.iter().all(|&[red, green, blue]| {
             red <= colours[0] && green <= colours[1] && blue <= colours[2]
         })
+    }
+
+    fn power(&self) -> u32 {
+        let minima = self.cubes.iter().fold([0, 0, 0], |minima, colours| {
+            [
+                minima[0].max(colours[0]),
+                minima[1].max(colours[1]),
+                minima[2].max(colours[2]),
+            ]
+        });
+        minima[0] * minima[1] * minima[2]
     }
 }
 
@@ -72,5 +93,11 @@ mod test {
     fn test1() {
         let input = fs::read("./test.txt").unwrap();
         assert_eq!(part1(&input), 8)
+    }
+
+    #[test]
+    fn test2() {
+        let input = fs::read("./test.txt").unwrap();
+        assert_eq!(part2(&input), 2286)
     }
 }
