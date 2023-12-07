1a921db85ff0042d5a2eae60cc6d87e39a451a75
SirBlueRabbit <68923537+SirBlueRabbit@users.noreply.github.com>
2023-12-07 17:02:29 +0100

    improve day 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -6,39 +6,24 @@ const DIGITS: [&str; 9] = [
 
 fn main() {
     let input = fs::read_to_string("./input.txt").unwrap();
-    let result1 = part1(&input);
-    let result2 = part2(&input);
+    let result1 = find_digits(&input, false);
+    let result2 = find_digits(&input, true);
 
     println!("The result of part 1 is: {result1}");
     println!("The result of part 2 is: {result2}");
 }
 
-fn part1(input: &str) -> u32 {
+fn find_digits(input: &str, part_two: bool) -> usize {
     input
         .lines()
         .map(|line| {
-            let mut digits = line.chars().filter_map(|char| char.to_digit(10));
-
-            let first = digits.next().unwrap_or(0);
-            let last = digits.last().unwrap_or(first);
-
-            10 * first + last
-        })
-        .sum()
-}
-
-fn part2(input: &str) -> u32 {
-    input
-        .lines()
-        .map(|line| {
-            let mut digits = line.chars().enumerate().filter_map(|(i, char)| {
-                char.to_digit(10).or({
-                    DIGITS
-                        .iter()
-                        .enumerate()
-                        .find(|(_, name)| line[i..].starts_with(**name))
-                        .map(|(n, _)| (n + 1) as u32)
-                })
+            let mut digits = line.chars().enumerate().filter_map(|(i, char)| match char {
+                '0'..='9' => char.to_digit(10).map(|n| n as usize),
+                _ if part_two => DIGITS
+                    .iter()
+                    .enumerate()
+                    .find_map(|(n, name)| line[i..].starts_with(*name).then_some(n + 1)),
+                _ => None,
             });
 
             let first = digits.next().unwrap_or(0);
@@ -56,12 +41,12 @@ mod test {
     #[test]
     fn test1() {
         let input = fs::read_to_string("./test1.txt").unwrap();
-        assert_eq!(part1(&input), 142)
+        assert_eq!(find_digits(&input, false), 142)
     }
 
     #[test]
     fn test2() {
         let input = fs::read_to_string("./test2.txt").unwrap();
-        assert_eq!(part2(&input), 281)
+        assert_eq!(find_digits(&input, true), 281)
     }
 }
