f1b1a6d5e74a03e2132e6d8800e0bfeca261ea1b
SirBlueRabbit <68923537+SirBlueRabbit@users.noreply.github.com>
2023-12-08 18:51:38 +0100

    day 4 part 1

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -3,18 +3,40 @@ use std::{fs, str};
 fn main() {
     let input = fs::read_to_string("./input.txt").unwrap();
     let result1 = part1(&input);
-    let result2 = part2(&input);
+    //let result2 = part2(&input);
 
     println!("The result of part 1 is: {result1}");
-    println!("The result of part 2 is: {result2}");
+    //println!("The result of part 2 is: {result2}");
 }
 
-fn part1(input: &str) -> u32 {
+fn part1(input: &str) -> usize {
+    input
+        .lines()
+        .map(parse)
+        .map(|(win, have)| score(win, have))
+        .sum()
+}
+
+fn part2(input: &str) -> usize {
     todo!();
 }
 
-fn part2(input: &str) -> u32 {
-    todo!();
+fn parse(line: &str) -> (Vec<usize>, Vec<usize>) {
+    let (_, card) = line.split_once(": ").unwrap();
+    let (win, have) = card.split_once(" | ").unwrap();
+    let win = win.split_whitespace().map(|n| n.parse().unwrap()).collect();
+    let have = have
+        .split_whitespace()
+        .map(|n| n.parse().unwrap())
+        .collect();
+    (win, have)
+}
+
+fn score(win: Vec<usize>, have: Vec<usize>) -> usize {
+    match have.iter().filter(|entry| win.contains(entry)).count() as u32 {
+        0 => 0,
+        n => 2_usize.pow(n - 1),
+    }
 }
 
 #[cfg(test)]
