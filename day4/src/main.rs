8cfb2d31ac18268d769b37665615dfec350a88b3
SirBlueRabbit <68923537+SirBlueRabbit@users.noreply.github.com>
2023-12-09 13:36:19 +0100

    day 4 finished

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,39 +1,45 @@
-use std::{fs, str};
+use std::{collections::VecDeque, fs, str};
 
 fn main() {
     let input = fs::read_to_string("./input.txt").unwrap();
     let result1 = part1(&input);
-    //let result2 = part2(&input);
+    let result2 = part2(&input);
 
     println!("The result of part 1 is: {result1}");
-    //println!("The result of part 2 is: {result2}");
+    println!("The result of part 2 is: {result2}");
 }
 
 fn part1(input: &str) -> usize {
-    input
-        .lines()
-        .map(parse)
-        .map(|(win, have)| score(win, have))
-        .sum()
+    input.lines().map(wins).map(score).sum()
 }
 
 fn part2(input: &str) -> usize {
-    todo!();
+    let mut buf = VecDeque::new();
+    input.lines().map(wins).fold(0, |counter, wins| {
+        let cards = 1 + buf.pop_front().unwrap_or(0);
+        (0..wins).for_each(|i| match buf.get_mut(i) {
+            Some(n) => *n += cards,
+            None => buf.push_back(cards),
+        });
+        counter + cards
+    })
 }
 
-fn parse(line: &str) -> (Vec<usize>, Vec<usize>) {
+fn wins(line: &str) -> usize {
     let (_, card) = line.split_once(": ").unwrap();
     let (win, have) = card.split_once(" | ").unwrap();
-    let win = win.split_whitespace().map(|n| n.parse().unwrap()).collect();
-    let have = have
+    let win = win
         .split_whitespace()
+        .map(|n| n.parse::<usize>().unwrap())
+        .collect::<Vec<_>>();
+    have.split_whitespace()
         .map(|n| n.parse().unwrap())
-        .collect();
-    (win, have)
+        .filter(|entry| win.contains(entry))
+        .count()
 }
 
-fn score(win: Vec<usize>, have: Vec<usize>) -> usize {
-    match have.iter().filter(|entry| win.contains(entry)).count() as u32 {
+fn score(wins: usize) -> usize {
+    match wins as u32 {
         0 => 0,
         n => 2_usize.pow(n - 1),
     }
@@ -52,6 +58,6 @@ mod test {
     #[test]
     fn test2() {
         let input = fs::read_to_string("./test.txt").unwrap();
-        assert_eq!(part2(&input), todo!())
+        assert_eq!(part2(&input), 30)
     }
 }
